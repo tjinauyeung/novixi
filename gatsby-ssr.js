dbf2a0008308f6ece182a850271e958b3946fc51
Tjin Au Yeung <tjinauyeung@gmail.com>
2020-08-04 21:31:25 +0200

    wrap element to prevent fouc

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <div {...props}>
+    {element}
+  </div>
+}
+
+exports.wrapRootElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <div {...props}>
+    {element}
+  </div>
+}
